2982a9f77e8a30631bc7fbe8d040c514dee6edc6
Frank Medvedik <fmedvedik@gmail.com>
2015-03-19 13:25:07 -0400

    switched guids for inbound agent

--- PhoneLogic.Core/ConditionalConfiguration.cs
+++ PhoneLogic.Core/ConditionalConfiguration.cs
@@ -32,17 +32,18 @@ namespace PhoneLogic.Core
      public static string LyncServiceRefUrl = "http://cc-app.reckner.com:1255/PhoneLogic";
      public const string RecknerCallAppGuid = "{E7D2695C-96F8-4C49-858A-28F6106B2B39}";
 #elif DEBUGTEST
-    public static string BuildType = "DEBUG-TEST";
+     public static string BuildType = "DEBUG-TEST";
      public static string apiUrl = "http://cc-app.reckner.com/inbound/api/";
      public static string rootUrl = "http://cc-app.reckner.com/inbound/";
      public static string LyncServiceRefUrl = "http://cc-app.reckner.com:1255/PhoneLogic";
-     public const string RecknerCallAppGuid = "{E7D2695C-96F8-4C49-858A-28F6106B2B39}";   
+     public const string RecknerCallAppGuid = "{EF327138-E9C6-4E5D-8BB3-F505DAB0F567}";
+        
 #elif DEBUGPROD
         public static string BuildType = "DEBUG-PROD";
         public static string apiUrl = "http://cc-prod.reckner.com/phoneroom/api/";
         public static string rootUrl = "http://cc-prod.reckner.com/phoneroom/";
         public static string LyncServiceRefUrl = "http://cc-prod.reckner.com:1255/PhoneLogic";
-        public const string RecknerCallAppGuid = "{EF327138-E9C6-4E5D-8BB3-F505DAB0F567}";
+     public const string RecknerCallAppGuid = "{E7D2695C-96F8-4C49-858A-28F6106B2B39}";   
 #elif RELEASEPROD
         public static string BuildType = "PROD-RELEASE";
         public static string apiUrl = "http://cc-prod.reckner.com/phoneroom/api/";
