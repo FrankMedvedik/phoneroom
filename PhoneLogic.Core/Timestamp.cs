75e6b307faa6bf2459a87d02f59605c285d96b19
Frank Medvedik <fmedvedik@gmail.com>
2015-05-06 19:36:24 -0400

    nailed it

--- PhoneLogic.Core/Timestamp.cs
+++ PhoneLogic.Core/Timestamp.cs
@@ -22,7 +22,7 @@ namespace PhoneLogic.Core
         /// <remarks>
         /// Do not modify the definition of BuildAt as your changes will be discarded.
         /// </remarks>
-        public static DateTime BuildAt { get { return new DateTime(635665310580737514); } } //--**
+        public static DateTime BuildAt { get { return new DateTime(635665374470243099); } } //--**
         /// <summary>
         /// The program that time stamped it.
         /// </summary>
